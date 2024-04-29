811d791e42b336414b1dc59b4a86525e1a1b9e53
ThienNoob <102866314+ThienNoob@users.noreply.github.com>
2024-04-29 09:09:38 +0700

    test jenkin

--- ThucHanh/TH_Lan_1/src/Bai1/Main.java
+++ ThucHanh/TH_Lan_1/src/Bai1/Main.java
@@ -27,17 +27,7 @@ public class Main {
         aNhanViens[luongCaoNhat(aNhanViens)].display();
 
     }
-    // public static int luongCaoNhat(NhanVien[] aNhanViens) {
-    //     int index = 0;
-    //     int max = aNhanViens[0].tinhLuong();
-    //     for (int i = 0; i < aNhanViens.length; i++) {
-    //         if (aNhanViens[i].tinhLuong() > max) {
-    //             max = aNhanViens[i].tinhLuong();
-    //             index = i;
-    //         }
-    //     }
-    //     return index;
-    // }
+   
 
     public static int tinhTongLuong(NhanVien[] aNhanViens) {
         int sum = 0;
